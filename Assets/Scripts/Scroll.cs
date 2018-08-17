2235d191cc8fab428fac19e5e98dd421ee53f34c
BENAVTABI-PC\BenAvtabi <ben.avtabi@gmail.com>
2018-08-17 18:56:01 +0300

    Scrolling script free testing

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour 
+{
+    private Rigidbody2D rb2d;
+    public float scrollSpeed;
+
+    // Use this for initialization
+    void Start () 
+    {
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //Start the object moving.
+        rb2d.velocity = new Vector2 (0, scrollSpeed);
+    }
+
+    void Update()
+    {
+        // If the game is over, stop scrolling.
+    }
+}
