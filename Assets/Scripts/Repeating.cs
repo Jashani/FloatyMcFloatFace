e67fbade62681685700af33b9d1e27d652f0b446
BENAVTABI-PC\BenAvtabi <ben.avtabi@gmail.com>
2018-08-17 19:26:13 +0300

    Scrolling Background now works

--- Assets/Scripts/Repeating.cs
+++ Assets/Scripts/Repeating.cs
@@ -5,16 +5,17 @@ using System;
 public class Repeating: MonoBehaviour 
 {
 
-    private BoxCollider2D groundCollider;       //This stores a reference to the collider attached to the Ground.
     private float groundVerticalLength;       //A float to store the x-axis length of the collider2D attached to the Ground GameObject.
+    public Camera mainCam;
+    private Transform cameratTrans;
 
     //Awake is called before Start.
     private void Awake ()
     {
-        //Get and store a reference to the collider2D attached to Ground.
-        groundCollider = GetComponent<BoxCollider2D> ();
         //Store the size of the collider along the x axis (its length in units).
-        groundVerticalLength = groundCollider.size.y;
+        groundVerticalLength =  GetComponent<BoxCollider2D>().size.y;
+        groundVerticalLength *= transform.localScale.y;
+        cameratTrans = mainCam.transform;
     }
 
     //Update runs once per frame
@@ -22,7 +23,7 @@ public class Repeating: MonoBehaviour
     {
         //Check if the difference along the y axis between the main Camera and the position of the object this is attached to is greater than groundHorizontalLength.
 
-        if (transform.position.y > 10*groundVerticalLength)
+        if (cameratTrans.position.y - transform.position.y > groundVerticalLength)
         {
             Debug.Log(String.Format("y: {0}, length {1}", transform.position.y, groundVerticalLength));
             //If true, this means this object is no longer visible and we can safely move it forward to be re-used.
@@ -34,7 +35,7 @@ public class Repeating: MonoBehaviour
     private void RepositionBackground()
     {
         //This is how far to the right we will move our background object, in this case, twice its length. This will position it directly to the right of the currently visible background object.
-        Vector2 groundOffSet = new Vector2(0, 10 * groundVerticalLength * -2f);
+        Vector2 groundOffSet = new Vector2(0, groundVerticalLength * 2f);
 
         //Move this object from it's position offscreen, behind the player, to the new position off-camera in front of the player.
         transform.position = (Vector2) transform.position + groundOffSet;
