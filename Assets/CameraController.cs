1fbd26c8ea156bb656da8c7e0b4e9c1cf6e0ce68
Jashani <jashaniyt@gmail.com>
2018-08-17 18:58:50 +0300

    Camera now doesn't drop with Floaty.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour {
 	public Rigidbody2D floaty;
 
 	private Vector3 velocity = Vector3.zero;
+	private float highestY = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,12 @@ public class CameraController : MonoBehaviour {
 
 	void FixedUpdate () {
 		Vector3 targetPosition = floaty.transform.TransformPoint (new Vector3 (0f, 1.5f, -10f));
+
+		if (highestY < targetPosition.y)
+			highestY = targetPosition.y;
+		else
+			targetPosition = new Vector3 (targetPosition.x, highestY, targetPosition.z);
+		
 		transform.position = Vector3.SmoothDamp (transform.position, targetPosition, ref velocity, 0.1f);
 	}
 }
