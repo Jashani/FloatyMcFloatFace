aa53412b38bae228674feff9b87a8944f1190cfc
Jashani <jashaniyt@gmail.com>
2018-08-17 18:26:20 +0300

    Added Floaty and he can now fly upwards with the press of a button.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	float maxSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			rb.AddForce (new Vector2 (0f, 15f));
+			Debug.Log ("Mouse down.");
+		}
+
+		if (rb.velocity.magnitude > maxSpeed) {
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+	}
+}
